9cd5299a477a54be76108b4fe4513deb7e869336
yasuobgg <yasuobgg@gmail.com>
2023-02-09 16:55:09 +0700

    first

---
+++ otp.py
@@ -0,0 +1,39 @@
+import random
+import time
+
+
+def set_random_otp():
+    digit = "123456789"
+    length = 6
+    otp = "".join(random.sample(digit, length))
+    return int(otp)
+
+
+# ham xoa otp sau 5 giay
+def destroy_otp(otp):
+    for sec in range(5, 0, -1):
+        print(sec)
+        time.sleep(1)
+    del otp
+    print("The OTP has been destroyed.")
+# def generate_and_destroy_otp():
+#     otp = set_random_otp()
+#     time.sleep(60)
+#     destroy_otp(otp)
+
+
+def verify_otp(user_otp, otp):
+    if not otp:
+        print("Invalid OTP")
+    if otp == user_otp:
+        print("Welcome user!")
+    else:
+        print("Access Denied!")
+
+    destroy_otp(otp)
+
+
+otp = set_random_otp()
+print("Hello, your OTP is:", str(otp))
+user_otp = int(input("Enter your OTP: "))
+verify_otp(user_otp, otp)
